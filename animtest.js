e50163ed0eff819b02100779bdaeaef0a80f206a
atahirli19 <124487351+atahirli19@users.noreply.github.com>
2023-10-01 15:07:10 +0400

    Add files via upload

---
+++ animtest.js
@@ -0,0 +1,60 @@
+//1
+// hide()
+
+//2
+// $('.block').click(function()
+
+// 3
+// show()
+
+// 4
+// $('.block').click(function(){
+//$(this).show()
+
+//5
+//(selector).show(4000)
+
+// 6
+// $(selector).hide(5000);
+
+// 7
+// $('button').click(function(){
+    //$(.block).hide)
+    //alert('cкрытo')  })
+
+//8
+// $('button').click(function(){
+    //$(.block).show()
+    //alert('pоказано') })
+
+// 9
+// $('button').click(function(){
+    //$(.block).fadeIn() })
+
+// 10
+// $('button').click(function(){
+    //$(.block).fadeOut() })
+
+// 11
+// $('button').click(function(){
+    //$(.block).fadeToggle()
+
+// 12
+// $('button').click(function(){
+    //$(.block).fadeTo(0)})
+
+// 13
+// $('button').click(function(){
+//$(.block).fadeIn(3000)})
+
+// 14
+// $('button').click(function(){
+    //$(.block).fadeOut(600) })
+
+// 15
+// $('button').click(function(){
+//$(.block).fadeToggle(8000) })
+
+// 16
+// $('button').click(function({
+//$(.block).fadeT(10000, 0) })
